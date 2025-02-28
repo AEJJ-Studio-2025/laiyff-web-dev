696d5cdbacdf01488a791ad4720d01b6122b8212
Jack-18888 <jack.shi.smart@gmail.com>
2025-02-28 11:43:14 -0500

    add home page template

--- home/models.py
+++ home/models.py
@@ -1,7 +1,38 @@
 from django.db import models
 
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from modelcluster.fields import ParentalKey
 
+
+
+class HomePageAccordion(Orderable):
+    """Model for storing title-content pairs in an accordion format."""
+    id = models.BigAutoField(primary_key=True)
+    page = ParentalKey("HomePage", related_name="accordion_items", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, help_text="Accordion Title")
+    content = RichTextField(blank=True, help_text="Accordion Content")
+
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("content"),
+    ]
+
+    def __str__(self):
+        return self.title
 
 class HomePage(Page):
-    pass
+    """Model for the Home Page with an accordion section."""
+    heroSectionVideo = models.FileField(upload_to="videos/", blank=True, null=True)
+    introText = models.TextField(blank=True, help_text="Description of Our Film Festival")
+
+    content_panels = Page.content_panels + [
+        FieldPanel('heroSectionVideo'),
+        FieldPanel('introText'),
+        InlinePanel('accordion_items', label="Accordion Sections"),  # Enables dynamic accordion items
+    ]
+
+    def get_accordion_pairs(self):
+        """Returns the accordion items as (title, content) pairs."""
+        return [(item.title, item.content) for item in self.accordion_items.all()]
