c538382de0f809e0c5ac0147b0354fb10aff15ba
EDYBIRD <edwardfang24@fmail.com>
2025-02-28 19:54:24 -0500

    website menus

---
+++ utilScripts/dbshell.sh
@@ -0,0 +1,5 @@
+
+cd ../
+source env/bin/activate
+
+python manage.py dbshell
--- utilScripts/makeMigrations.sh
+++ utilScripts/makeMigrations.sh
@@ -1,6 +1,7 @@
 cd ../
 source env/bin/activate
 
+# python manage.py migrate --fake wagtailmenus zero
+
 python manage.py makemigrations
-python manage.py migrate --fake
 python manage.py migrate
