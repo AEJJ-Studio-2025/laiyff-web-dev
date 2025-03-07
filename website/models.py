00a6ffa51b10391357f2fefef093c785da4522e9
Jack-18888 <jack.shi.smart@gmail.com>
2025-03-06 21:16:28 -0500

    add background video, image and carousel option to normal page without using hero section block

--- website/models.py
+++ website/models.py
@@ -1,18 +1,53 @@
 from django.db import models
 from wagtail.models import Page
-from wagtail.fields import StreamField
-from wagtail.admin.panels import FieldPanel
-from .blocks import HeroSectionBlock, ContentBlock
+from wagtail.fields import StreamField, RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from .blocks import ContentBlock
+from modelcluster.fields import ParentalKey
+from wagtail.images.models import Image
+
+
+
+class CarouselImages(models.Model):
+    Page = ParentalKey("NormalPage", on_delete=models.CASCADE, related_name="carousel_images")
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="+")
+
+    panels = [
+        FieldPanel("image"),
+    ]  
 
 class NormalPage(Page):
     """A page model that allows only 0 or 1 Hero Section but unlimited other components"""
 
+    heroTitle = models.TextField(blank=True, help_text="Description of Our Film Festival")
+    body = RichTextField(blank=True)
+
+    HERO_CHOICES = [
+        ('Image', 'Use background image'),
+        ('Video', 'Use background video'),
+        ('Carousel', 'Carousel'),
+    ]
+
+    heroType = models.CharField(
+        max_length=20,
+        choices=HERO_CHOICES,
+        default='option1',
+    )
+
+    heroSectionVideo = models.FileField(upload_to="videos/", blank=True, null=True)
+    heroSectionImage = models.FileField(upload_to="images/", blank=True, null=True)
+
     components = StreamField([
-        ('hero_section', HeroSectionBlock()),  
         ('content_block', ContentBlock()),  
     ], use_json_field=True, blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel("heroTitle"),
+        FieldPanel("body"),
+        FieldPanel("heroType"),
+        FieldPanel("heroSectionVideo"),
+        FieldPanel("heroSectionImage"),
+        InlinePanel("carousel_images", label="Carousel Images"),
         FieldPanel("components"),
     ]
 
