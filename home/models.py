a81af4d11e107a19a1e404abd5a03ebd4df7e9e8
Jack-18888 <jack.shi.smart@gmail.com>
2025-03-24 22:13:25 -0400

    added film display block to home page

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 from wagtail.models import Page, Orderable
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel, InlinePanel
 from modelcluster.fields import ParentalKey
+from .blocks import FilmDisplayBlock
 
 
 class HomePageAccordion(Orderable):
@@ -30,12 +31,16 @@ class HomePage(Page):
     heroSectionVideo = models.FileField(upload_to="videos/", blank=True, null=True)
     introText = models.TextField(blank=True, help_text="Description of Our Film Festival")
     body = RichTextField(blank=True)
+    films = StreamField([
+        ('film', FilmDisplayBlock()),
+    ], blank=True, use_json_field=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('heroSectionVideo'),
         FieldPanel('introText'),
-        InlinePanel('accordion_items', label="Accordion Sections"),  # Enables dynamic accordion items
+        InlinePanel('accordion_items', label="Accordion Sections"), 
         FieldPanel('body'),
+        FieldPanel('films'),
     ]
 
     def get_accordion_pairs(self):
