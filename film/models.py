14526db9b690e02e9ff737a7a1ebdee3c75c783a
Jack-18888 <jack.shi.smart@gmail.com>
2025-03-30 00:29:26 -0400

    create film model

--- film/models.py
+++ film/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+
+class FilmPage(Page):
+    name = models.CharField(blank=True, help_text="Film Title", max_length=512)
+    description = RichTextField(blank=True, help_text="Film Description")
+    director = models.CharField(blank=True, help_text="Film Director", max_length=200)
+    released_year = models.PositiveIntegerField(help_text="Released Date", default=2025)
+    genres = models.CharField(
+        blank=True,
+        help_text="Comma-separated genres (e.g. Sci-Fi, Thriller)",
+        max_length=512,
+        default=""
+    )
+
+    photo = models.FileField(upload_to="images/", blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("name"), 
+        FieldPanel("description"),
+        FieldPanel("director"),
+        FieldPanel("released_year"),
+        FieldPanel("genres"),
+        FieldPanel("photo"),
+    ]
