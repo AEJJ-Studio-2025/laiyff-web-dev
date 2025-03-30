4d3f7f07ef6c5d5894be8244a4d46ba2f08d5425
Jack-18888 <jack.shi.smart@gmail.com>
2025-03-30 15:07:46 -0400

    add carousel style hero display for home page template

--- home/models.py
+++ home/models.py
@@ -1,31 +1,25 @@
 from django.db import models
 
-from wagtail.models import Page, Orderable
-from wagtail.fields import RichTextField, StreamField
+from wagtail.models import Page
+from wagtail.fields import StreamField
 from wagtail.admin.panels import FieldPanel, InlinePanel
 from modelcluster.fields import ParentalKey
 from .blocks import FilmDisplayBlock
+from wagtail.images.models import Image
 
 
-class HomePageAccordion(Orderable):
-    """Model for storing title-content pairs in an accordion format."""
+class CarouselImages(models.Model):
+    Page = ParentalKey("HomePage", on_delete=models.CASCADE, related_name="carousel_images")
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="+")
     id = models.BigAutoField(primary_key=True)
-    page = ParentalKey("HomePage", related_name="accordion_items", on_delete=models.CASCADE)
-    title = models.CharField(max_length=255, help_text="Accordion Title")
-    content = RichTextField(blank=True, help_text="Accordion Content")
 
     panels = [
-        FieldPanel("title"),
-        FieldPanel("content"),
-    ]
-
-    def __str__(self):
-        return self.title
+        FieldPanel("image"),
+    ]  
 
 
 class HomePage(Page):
     """Model for the Home Page with an accordion section."""
-    heroSectionVideo = models.FileField(upload_to="videos/", blank=True, null=True)
     heroText = models.CharField(help_text="Title of the Page", default="Welcome to Los Angeles International Film Festival", max_length=200)
     introText = models.TextField(blank=True, help_text="Description of Our Film Festival")
     films = StreamField([
@@ -33,7 +27,7 @@ class HomePage(Page):
     ], blank=True, use_json_field=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('heroSectionVideo'),
+        InlinePanel("carousel_images", label="Carousel Images"),
         FieldPanel('heroText'),
         FieldPanel('introText'),
         FieldPanel('films'),
